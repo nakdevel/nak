f4341846355cc1dc39feb52019b190280b637527
nakdevel <nakdevel@gmail.com>
2015-03-23 01:22:23 -0300

    Main Routing Added

--- nak.js
+++ nak.js
@@ -9,3 +9,27 @@ app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+    res.render('index');
+});
+
+
+// 404
+app.use(function(req, res, next){
+    res.status(404);
+    res.render('404');
+});
+
+// 500
+app.use(function(err, req, res, next){
+    console.error(err.stack);
+    res.status(500);
+    res.render('500');
+});
+
+
+// Start the Server
+app.listen(app.get('port'), function() {
+    console.log('Nak running on http://localhost:' + app.get('port'));
+});
